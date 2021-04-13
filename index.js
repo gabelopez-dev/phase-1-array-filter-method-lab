d2d9c822645863a2e7f499d43a8364f88cd3ede3
Gabriel Lopez <gabelopez84@gmail.com>
2021-04-13 15:40:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 // Code your solution here
-findMatching(function () 
-describe('index.js', function () {
-    describe('findMatching()', function () {
-      it('returns all drivers that match the passed in name', function () {
-        const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-  
-        expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'Bobby']);
-        expect(findMatching(drivers, 'Sammy')).to.eql(['Sammy']);
-      });
+findMatching();
